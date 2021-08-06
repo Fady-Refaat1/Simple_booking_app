05b24386c9540bc5a22b551f272c9ab8dc0d96db
Fady Refaat <fadyrefaatzak@gmail.com>
2021-08-06 16:47:12 +0200

    add middleWares

---
+++ src/MiddleWares/logger.js
@@ -0,0 +1,10 @@
+export const logger = (store)=>(next)=>(action)=>{
+
+    console.group(action.type)
+    console.log('Action : ',action)
+    const toNext = next(action)
+    console.log('New state is : ' , store.getState())
+    console.groupEnd()
+
+    return toNext;
+}
